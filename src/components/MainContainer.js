2b453c5f1b901c707ddd1db2a73c6af0ea1e2196
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-05-20 14:27:44 -0400

    feat: add ability to upload files to cloudinary

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+// import { Image, Transformation, CloudinaryContext } from 'cloudinary-react';
 
-import { IDEAS } from '../data/constants';
+import { IDEAS, 
+  // CLOUDINARY_CLOUD_NAME, 
+} from '../data/constants';
 
 import Idea from './Idea';
 import GeneratorBar from './GeneratorBar';
+import AddContent from './AddContent';
 
 const MainContainer = ({ideaId, setCurrentIdeaId, setIdeasData}) => {
 
@@ -31,6 +35,8 @@ const MainContainer = ({ideaId, setCurrentIdeaId, setIdeasData}) => {
     }
   },[ideaId]);
 
+  // const image_height = 300;
+
   return (
     <div id="main-content">
       <GeneratorBar setCurrentIdeaId={setCurrentIdeaId} setIdeasData={setIdeasData} />
@@ -41,8 +47,19 @@ const MainContainer = ({ideaId, setCurrentIdeaId, setIdeasData}) => {
           : <p>Loading idea...</p>
         : <p>Generate a new idea above, or select one from history on left.</p>
       }
+
+      {/* <CloudinaryContext cloudName={CLOUDINARY_CLOUD_NAME}>
+        <Image publicId="samples/landscapes/nature-mountains" height={image_height} onLoad={() => console.log('loaded!')} >
+          <Transformation height={image_height * 2} crop="scale" quality="auto" fetchFormat="auto" />
+        </Image>
+      </CloudinaryContext> */}
+
+      <AddContent />
     </div>
   );
 }
 
 export default MainContainer;
+
+// const image_height = 300;
+// <img height={image_height} src={`http://res.cloudinary.com/badideas/image/upload/h_${image_height * 2},f_auto,q_auto/v1/samples/landscapes/nature-mountains`} alt=""/>
