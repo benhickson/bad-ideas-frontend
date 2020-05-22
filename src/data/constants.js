f05f84cf9d35aaef545a4729d020ccd8505a7a85
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-05-22 13:17:20 -0400

    ci: make the API endpoint change depending on environment variable

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,10 @@
-// const API_ENDPOINT = 'http://localhost:3000/api/v1';
-const API_ENDPOINT = 'https://bad-ideas-backend.herokuapp.com/api/v1';
+let API_ENDPOINT;
+if (process.env.NODE_ENV !== 'production') {
+  API_ENDPOINT = 'http://localhost:3000/api/v1';
+} else {
+  API_ENDPOINT = 'https://bad-ideas-backend.herokuapp.com/api/v1';
+}
+
 const IDEAS =         API_ENDPOINT+'/ideas';
 const RECENT_IDEAS =  API_ENDPOINT+'/ideas/recent';
 const LOGIN =         API_ENDPOINT+'/login';
